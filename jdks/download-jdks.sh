7fbe83c2c31d094322108980a94574d85a5d6478
Toni Helenius <helenius.toni@gmail.com>
2022-05-01 20:11:10 +0300

    Jdk script (#323)
    * Switch to Temurin
    
    * Fix escapes

--- jdks/download-jdks.sh
+++ jdks/download-jdks.sh
@@ -11,8 +11,8 @@
 set -e # Quit on Error
 
 jdk_major_version="11"
-jdk_version="0.6"
-jdk_build_version="10"
+jdk_version="0.14.1"
+jdk_build_version="1"
 platforms=( "x64_linux" "x86-32_windows" "x64_windows" "x64_mac" )
 
 # DEPRECATED (not required anymore)
@@ -72,12 +72,7 @@ function download_jdk {
     then
         echo "<<< Already existing, SKIPPING."
     else
-        if [ "$jdk_major_version" == "8" ];
-        then
-            curl -s -o downloads/jdk-$1$2 -L https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk$jdk_version-$jdk_build_version/OpenJDK8U-jdk_$1_hotspot_$jdk_version$jdk_build_version$2
-        else
-            curl -s -o downloads/jdk-$1$2 -L https://github.com/AdoptOpenJDK/openjdk$jdk_major_version-binaries/releases/download/jdk-$jdk_major_version.$jdk_version+$jdk_build_version/OpenJDK$jdk_major_version\U-jdk_$1_hotspot_$jdk_major_version.$jdk_version\_$jdk_build_version$2
-        fi
+        curl -s -o downloads/jdk-$1$2 -L https://github.com/adoptium/temurin$jdk_major_version-binaries/releases/download/jdk-$jdk_major_version.$jdk_version+$jdk_build_version/OpenJDK${jdk_major_version}U-jdk_$1_hotspot_$jdk_major_version.${jdk_version}_$jdk_build_version$2
         echo "<<< OK!"
     fi
 }
