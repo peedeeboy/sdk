dd6f15157cec52969017544fc073cdc8b5db815f
rickard <rickard@mindemia.com>
2022-09-23 21:30:34 +0200

    icons for assets - texture, material

---
+++ jme3-texture-editor/src/com/jme3/gde/textureeditor/icons/Icons.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jme3.gde.textureeditor.icons;
+
+/**
+ *
+ * @author rickard
+ */
+public class Icons {
+    
+    public static final String ICONS_PATH = "com/jme3/gde/textureeditor/icons/";
+    public static final String TEXTURE_GREEN = ICONS_PATH + "brick-wall_green.svg";
+    public static final String TEXTURE_BLUE = ICONS_PATH + "brick-wall_blue.svg";
+    
+}
