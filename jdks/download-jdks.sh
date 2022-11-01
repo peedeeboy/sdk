00cec129e4ef27c9ccb443d89fc7e49c7c87a7eb
Toni Helenius <helenius.toni@gmail.com>
2022-11-01 17:55:45 +0200

    Pick up new JDK version

--- jdks/download-jdks.sh
+++ jdks/download-jdks.sh
@@ -11,8 +11,8 @@
 set -e # Quit on Error
 
 jdk_major_version="11"
-jdk_version="0.15"
-jdk_build_version="10"
+jdk_version="0.17"
+jdk_build_version="8"
 platforms=( "x64_linux" "x86-32_windows" "x64_windows" "x64_mac" )
 
 # DEPRECATED (not required anymore)
