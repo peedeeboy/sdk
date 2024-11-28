b41846ec722fae583cc5fa05585f39b78994ce13
rickard <rickard@mindemia.com>
2024-11-28 18:54:56 +0100

    simplify methods, thanks to capdevon

--- jme3-core/src/com/jme3/gde/core/assets/actions/MergeAnimationsAction.java
+++ jme3-core/src/com/jme3/gde/core/assets/actions/MergeAnimationsAction.java
@@ -42,28 +42,28 @@ import com.jme3.anim.util.HasLocalTransform;
 import com.jme3.gde.core.assets.SpatialAssetDataObject;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.plugins.bvh.SkeletonMapping;
+import com.jme3.util.SafeArrayList;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import jme3utilities.MyAnimation;
-import jme3utilities.wes.AnimationEdit;
-import org.bushe.swing.event.Logger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
 import org.openide.NotifyDescriptor.Confirmation;
 import org.openide.util.Exceptions;
 
 /**
- * Action for merging one or more spatials' animation to another.
- * Same rig required.
+ * Action for merging one or more spatials' animation to another. Same rig
+ * required.
+ *
  * @author rickard
  */
 public class MergeAnimationsAction implements ActionListener {
-    
+
     private static final String CANCEL = "Cancel";
     private final List<SpatialAssetDataObject> spatials;
     private final Logger logger;
@@ -77,84 +77,63 @@ public class MergeAnimationsAction implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (spatials.size() == 1) {
             DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(
-                            "Must select more than one spatial",
-                            NotifyDescriptor.ERROR_MESSAGE));
+                    "Must select more than one spatial",
+                    NotifyDescriptor.ERROR_MESSAGE));
             return;
         }
-        
+
         final Object selectedSpatial = createSelector().getValue();
-        
+
         if (selectedSpatial == null || selectedSpatial.equals(CANCEL)) {
-            logger.log(Logger.Level.INFO, "Operation cancelled by user.");
+            logger.log(Level.INFO, "Operation cancelled by user.");
             return;
         }
-        
+
         final SpatialAssetDataObject targetAsset = findSpatial(selectedSpatial.toString());
-        
+
         if (targetAsset == null) {
-            logger.log(Logger.Level.INFO, "Operation failed. No spatial.");
+            logger.log(Level.INFO, "Operation failed. No spatial.");
             return;
         }
-        
+
         final Spatial targetSpatial = targetAsset.loadAsset();
         final AnimComposer targetAnimComposer = findAnimComposer(targetSpatial, null);
-        
+
         if (targetAnimComposer == null) {
             DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(
-                            String.format("%s has no AnimComposer.", targetSpatial),
-                            NotifyDescriptor.ERROR_MESSAGE));
+                    String.format("%s has no AnimComposer.", targetSpatial),
+                    NotifyDescriptor.ERROR_MESSAGE));
             return;
         }
         final Spatial targetAnimComposerSpatial = targetAnimComposer.getSpatial();
         for (final Iterator<SpatialAssetDataObject> it = spatials.iterator(); it.hasNext();) {
             final SpatialAssetDataObject spatialAssetDataObject = it.next();
-            if(spatialAssetDataObject.getName().equals(selectedSpatial)) {
+            if (spatialAssetDataObject.getName().equals(selectedSpatial)) {
                 continue;
             }
             Spatial sourceSpatial = spatialAssetDataObject.loadAsset();
             final AnimComposer sourceAnimComposer = findAnimComposer(sourceSpatial, null);
             if (sourceAnimComposer == null) {
                 DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(
-                            String.format("%s has no AnimComposer.", sourceSpatial),
-                            NotifyDescriptor.ERROR_MESSAGE));
+                        String.format("%s has no AnimComposer.", sourceSpatial),
+                        NotifyDescriptor.ERROR_MESSAGE));
                 return;
             }
             copyClips(sourceAnimComposer, targetAnimComposer, targetAnimComposerSpatial.getControl(SkinningControl.class).getArmature());
         }
-        logger.log(Logger.Level.INFO, "Merging animations done. Saving.");
+        logger.log(Level.INFO, "Merging animations done. Saving.");
         try {
             targetAsset.saveAsset();
         } catch (IOException ex) {
             Exceptions.printStackTrace(ex);
         }
     }
-    
-    private static AnimClip retargetClip(
-            AnimClip clip, Armature armature, String clipName) {
-        AnimTrack[] animTracks = clip.getTracks();
-        AnimClip result = new AnimClip(clipName);
-        for (AnimTrack animTrack : animTracks) {
-            if (MyAnimation.isJointTrack(animTrack)) {
-                TransformTrack transformTrack = (TransformTrack) animTrack;
-                HasLocalTransform animTarget = transformTrack.getTarget();
-                Joint animJoint = (Joint) animTarget;
-                String jointName = animJoint.getName();
-                Joint charaJoint = armature.getJoint(jointName);
-                if (charaJoint != null) {
-                    transformTrack.setTarget(charaJoint);
-                    AnimationEdit.addTrack(result, transformTrack);
-                }
-            }
-        }
-        return result;
-    }
 
-    
     private void copyClips(final AnimComposer from, final AnimComposer to, Armature toArmature) {
         final Collection<AnimClip> animClips = from.getAnimClips();
-        for(AnimClip animClip: animClips) {
+        for (AnimClip animClip : animClips) {
             to.addAnimClip(retargetClip(animClip, toArmature, animClip.getName()));
-            logger.log(Logger.Level.DEBUG, String.format("Added anim clip %s", animClip.getName()));
+            logger.log(Level.FINE, String.format("Added anim clip %s", animClip.getName()));
         }
     }
 
@@ -166,22 +145,22 @@ public class MergeAnimationsAction implements ActionListener {
             spatials[index++] = spatialAssetDataObject.getName();
         }
         spatials[index++] = CANCEL;
-        final NotifyDescriptor.Confirmation message =
-                new NotifyDescriptor.Confirmation(
+        final NotifyDescriptor.Confirmation message
+                = new NotifyDescriptor.Confirmation(
                         "Select spatial to copy animations to.");
         message.setOptions(spatials);
-        
+
         DialogDisplayer.getDefault().notify(message);
-        
+
         return message;
     }
-    
+
     private AnimComposer findAnimComposer(Spatial spatial, AnimComposer animComposer) {
         if (spatial.getControl(AnimComposer.class) != null) {
-            animComposer = spatial.getControl(AnimComposer.class);
+            return spatial.getControl(AnimComposer.class);
         }
         if (animComposer == null && spatial instanceof Node node) {
-            for (Spatial child: node.getChildren()) {
+            for (Spatial child : node.getChildren()) {
                 animComposer = findAnimComposer(child, animComposer);
                 if (animComposer != null) {
                     return animComposer;
@@ -190,19 +169,65 @@ public class MergeAnimationsAction implements ActionListener {
         }
         return animComposer;
     }
-    
+
     private SpatialAssetDataObject findSpatial(String selected) {
         for (Iterator<SpatialAssetDataObject> it = spatials.iterator(); it.hasNext();) {
             final SpatialAssetDataObject spatialAssetDataObject = it.next();
-            if(spatialAssetDataObject.getName().equals(selected)) {
+            if (spatialAssetDataObject.getName().equals(selected)) {
                 return spatialAssetDataObject;
             }
         }
         NotifyDescriptor.Message msg = new NotifyDescriptor.Message(
-            "Main asset to copy to not found. This is likely an issue with the tool itself.",
-            NotifyDescriptor.ERROR_MESSAGE);
+                "Main asset to copy to not found. This is likely an issue with the tool itself.",
+                NotifyDescriptor.ERROR_MESSAGE);
         DialogDisplayer.getDefault().notify(msg);
         return null;
     }
-    
+
+    private AnimClip retargetClip(AnimClip sourceClip, Armature targetArmature, String clipName) {
+
+        // Create a list to hold the new tracks
+        SafeArrayList<AnimTrack> tracks = new SafeArrayList<>(AnimTrack.class);
+
+        // Iterate through each track in the source clip
+        for (AnimTrack animTrack : sourceClip.getTracks()) {
+
+            TransformTrack sourceTrack = (TransformTrack) animTrack;
+            String targetName = getTargetName(sourceTrack.getTarget());
+            if (targetName == null) {
+                logger.log(Level.SEVERE, String.format("Unsupported target for: %s. Skipping.", animTrack));
+                continue;
+            }
+            Joint target = targetArmature.getJoint(targetName);
+
+            if (target != null) {
+                // Clone the source track and set the new target joint
+                TransformTrack newTrack = sourceTrack.jmeClone();
+                newTrack.setTarget(target);
+                tracks.add(newTrack);
+
+            } else {
+                logger.log(Level.WARNING, "Joint not found in the target Armature: {0}", targetName);
+            }
+        }
+
+        // Create a new animation clip with the specified name and set its tracks
+        AnimClip newClip = new AnimClip(clipName);
+        newClip.setTracks(tracks.getArray());
+
+        logger.log(Level.INFO, "Created new AnimClip {0} with {1} tracks out of {2} from the source clip",
+                new Object[]{clipName, tracks.size(), sourceClip.getTracks().length});
+        return newClip;
+    }
+
+    private String getTargetName(final HasLocalTransform target) {
+        if (target instanceof Node node) {
+            return node.getName();
+        }
+        if (target instanceof Joint joint) {
+            return joint.getName();
+        }
+        return null;
+    }
+
 }
